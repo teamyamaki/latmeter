9bc2fd1341172af6928c9798df166492e4be1d5d
otsuka <otti00437@gmail.com>
2013-09-28 22:30:19 +0900

    test

--- routes/taxi.js
+++ routes/taxi.js
@@ -12,15 +12,6 @@ exports.add = function(req, res) {
 
 exports.create = function(req, res) {
 
-//	var geocoder = require('geocoder');
-//	// 逆ジオコーディング(座標から住所を得る)
-//	geocoder.reverseGeocode(35.6480801, 139.7416143, function ( err, data ) {
-//		// 任意の処理
-//		console.log(data);
-//	});
-
-	getLocationNames();
-	
   var taxi = new Taxi(req.body);
   taxi.save(function() {
     res.redirect('/');
@@ -31,29 +22,3 @@ exports.create = function(req, res) {
     geoLocation.save();
   }
 };
-
-function getLocationName () {
-
-    return name;
-}
-
-function getLocationNames () {
-	var geocoder = require('geocoder');
-	// 逆ジオコーディング(座標から住所を得る)
-	setTimeout(geocoder.reverseGeocode(35.6480801, 139.7416143, function ( err, data ) {
-		// 任意の処理
-
-    var address_components = data.results;
-    console.log("■１ = " + address_components);
-
-	var address_component = address_components[0];	
-    console.log("■２" + address_component);
-
-    var name = address_component.formatted_address;
-    console.log("■３" + name);
-		
-		
-		
-	}), 5000);
-}
-
