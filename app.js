d40af5e51d3a0b8464ba1b4930e238dca426ae68
otsuka <otti00437@gmail.com>
2013-09-28 18:15:19 +0900

    ロケーション情報追加

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var routes = require('./routes');
 var taxi = require('./routes/taxi');
+var geoLocation = require('./routes/geoLocation');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
@@ -34,6 +35,8 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/taxi', taxi.index);
 app.post('/taxi', taxi.create);
+app.get('/geoLocation', geoLocation.index);
+app.post('/geoLocation', geoLocation.create);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
