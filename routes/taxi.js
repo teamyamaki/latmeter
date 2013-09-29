4c61c9c736fe8aaa2ed0b8f4a6f0716a386546ac
ichy <ichylinux@gmail.com>
2013-09-29 12:06:22 +0900

    リファクタリング

--- routes/taxi.js
+++ routes/taxi.js
@@ -18,7 +18,7 @@ exports.create = function(req, res) {
     res.redirect('/');
   });
 
-  if (req.body.latitude && req.body.longitude) {
+  if (true) {//req.body.latitude && req.body.longitude) {
     console.log('位置情報を保存');
     var geoLocation = new GeoLocation(req.body);
     geoLocation.createdAt = new Date();
