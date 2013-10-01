6a416b37e914b4d6c5ffc03046a4e3bf6dd7604a
良平 <@dev-box>
2013-10-02 00:30:03 +0900

    登録した経路を表示できるようにした。

--- routes/geoLocation.js
+++ routes/geoLocation.js
@@ -1,80 +1,90 @@
 require('../models/geoLocation');
 
 exports.index = function(req, res) {
-  var ridingId = req.query.ridingId;
-  
-  if (ridingId) {
-    GeoLocation.findByRidingId(ridingId, function(geoLocations) {
-      console.log("経路情報： " + geoLocations.length + "件");
-      
-      var geoLocs = [];
-      
-      geoLocs[0] = new GeoLocation({
-    	  latitude: 35.682259,
-     	  longitude:139.870548,
-     	  ridingId :ridingId,
-     	  createdAt:new Date()
-      });
-      
-      geoLocs[1] = new GeoLocation({
-    	  latitude: 35.677936,
-     	  longitude:139.858238,
-     	 ridingId :ridingId,
-     	createdAt:new Date()
-      });
-      
-      geoLocs[2] = new GeoLocation({
-    	  latitude: 35.677936,
-     	  longitude:139.828238,
-     	 ridingId :ridingId,
-     	createdAt:new Date()
-      });
-      
-      geoLocs[3] = new GeoLocation({
-    	  latitude: 35.677936,
-     	  longitude:139.798238,
-     	 ridingId :ridingId,
-     	createdAt:new Date()
-      });
-      
-      geoLocs[4] = new GeoLocation({
-    	  latitude: 35.677936,
-     	  longitude:139.768238,
-     	 ridingId :ridingId,
-     	createdAt:new Date()
-      });
-      
-      console.log("経路情報： " + geoLocs.length + "件");
-      
-      res.render('geoLocation/index', { geoLocations : geoLocs});
-    });
-  }else{
-	  res.render('geoLocation/index', { geoLocations : ''});
-  }
+	var ridingId = req.query.ridingId;
+
+	if (ridingId) {
+		GeoLocation.findByRidingId(ridingId, function(geoLocations) {
+			console.log(ridingId);
+			if(ridingId == 'dummy'){
+				
+				//テスト用
+				var geoLocs = [];
+				
+				geoLocs[0] = new GeoLocation({
+					latitude: 35.607936,
+					longitude:139.888238,
+					ridingId :ridingId,
+					createdAt:new Date()
+				});
+
+				geoLocs[1] = new GeoLocation({
+					latitude: 35.677936,
+					longitude:139.858238,
+					ridingId :ridingId,
+					createdAt:new Date()
+				});
+
+				geoLocs[2] = new GeoLocation({
+					latitude: 35.677936,
+					longitude:139.828238,
+					ridingId :ridingId,
+					createdAt:new Date()
+				});
+
+				geoLocs[3] = new GeoLocation({
+					latitude: 35.677936,
+					longitude:139.798238,
+					ridingId :ridingId,
+					createdAt:new Date()
+				});
+
+				geoLocs[4] = new GeoLocation({
+					latitude: 35.677936,
+					longitude:139.768238,
+					ridingId :ridingId,
+					createdAt:new Date()
+				});
+
+				console.log("経路情報(dummy)： " + geoLocs.length + "件");
+
+				res.render('geoLocation/index', { geoLocations : geoLocs});
+
+			}else{
+
+				console.log("経路情報： " + geoLocations.length + "件");
+
+				res.render('geoLocation/index', { geoLocations : geoLocations});
+			}
+
+		});
+	}else{
+		res.render('geoLocation/index', { geoLocations : ''});
+	}
 };
 
 exports.create = function(req, res) {
-	
+
 	console.log("Posted geoLocation: " + req.body);
-	
-  geoLocation = new GeoLocation(req.body);
-  
-  geoLocation.createdAt = new Date();
-  geoLocation.save(function() {
-    res.redirect('/');
-  });
+
+	geoLocation = new GeoLocation(req.body);
+
+	geoLocation.createdAt = new Date();
+	geoLocation.save(function() {
+		res.redirect('/');
+	});
 };
-  
+
 exports.mp = function(req, res) {
-    console.log("=■======:exports.map " + req.query	);
-    
-    　	for (key in req.query) {
-	    console.log(key + ":" + req.query[key]);
-　	}
+	console.log("=■======:exports.map " + req.query	);
+
+	for (key in req.query) {
+		console.log(key + ":" + req.query[key]);
+	}
 //	geoLocation = new GeoLocation(req.query);
-  	GeoLocation.findByRidingId(req.query, function(docs) {
+	GeoLocation.findByRidingId(req.query, function(docs) {
 		// 位置情報のリストが返却されるはず
 		// TODO
 		console.log(docs.length);
-  	});
+	});
 };
